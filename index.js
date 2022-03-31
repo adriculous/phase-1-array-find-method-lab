0dc2ab671740a8ca630d9fcad95bd79ab14cdb0f
adriculous <admarpad@gmail.com>
2022-03-30 19:07:42 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+// Practice using find() to Isolate a Specific Result
+
+const record = [
+  { year: "2018", result: "L" },
+  { year: "2017", result: "W" },
+  { year: "2016", result: "N/A" },
+];
+
+function superbowlWin(record) {
+  function winningYear(year) {
+    if (year.result === "W") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  const victoryYears = record.find(winningYear);
+  if (victoryYears) {
+    return victoryYears.year;
+  }
+}
